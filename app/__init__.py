28c4d1d1e367f45a668f94e604998a66d5a3bd3a
Minh Nguyen <hongminh1620@gmail.com>
2020-05-12 10:53:01 +0700

    create init file

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+db = SQLAlchemy(app)
