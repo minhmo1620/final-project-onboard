d01588ec6df58563d8821e52a7803ea276309188
Mia Nguyen <mia@Mias-MacBook-Pro.local>
2020-05-14 00:27:11 +0700

    create CategoryModel

---
+++ app/models/categories.py
@@ -0,0 +1,25 @@
+from db import db
+
+class CategoryModel(db.Model):
+	__tablename__ = 'categories'
+
+	id = db.Column(db.Integer, primary_key=True)
+	category = db.Column(db.String(100))
+	creator_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+	users = db.relationship('UserModel')
+
+	def __init__(self, category, creator_id):
+		self.category = category
+		self.creator_id = creator_id
+
+	def json(self):
+		return {'category': self.category}
+
+	@classmethod
+	def find_by_name(cls, name):
+		return cls.query.filter_by(category=name).first() #SELECT * FROM items WHERE name=name LIMIT=1
+
+	def save_to_db(self):
+		db.session.add(self)
+		db.session.commit()
