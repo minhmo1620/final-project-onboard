ebed00c702594d05e9ef762bc1e36cb91420e283
Minh Nguyen <hongminh1620@gmail.com>
2020-05-15 08:48:12 +0700

    updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,19 @@
 import os
-# import yaml
+
+from flask_marshmallow import Marshmallow
 
 from flask import Flask, Blueprint
-from flask_sqlalchemy import SQLAlchemy
 from flask_jwt import JWT
-from flask_mysqldb import MySQL
-import mysql.connector
 from .securtity import authenticate, identity
 
 from .db import db
 from .models.users import UserModel
 from .models.items import ItemModel
 from .models.categories import CategoryModel
+
+from .controller.items import items
+from .controller.categories import categories
+from .controller.auth import auth
 
 
 def create_app():
@@ -24,10 +26,6 @@ def create_app():
     db.init_app(app)
 
     with app.app_context():
-        from .controller.items import items
-        from .controller.categories import categories
-        from .controller.auth import auth
-
         app.register_blueprint(auth, url_prefix="")
         app.register_blueprint(categories, url_prefix="/categories")
         app.register_blueprint(items, url_prefix="/categories/<int:category_id>/items")
@@ -41,7 +39,7 @@ def create_app():
 
 app = create_app()
 app.app_context().push()
-
+ma = Marshmallow(app)
 
 
 
