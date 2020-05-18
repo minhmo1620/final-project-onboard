7ef5582b02cec4c61a5251c7521faf86ea51a7e2
Minh Nguyen <hongminh1620@gmail.com>
2020-05-18 07:36:02 +0700

    move config folder

--- config/local.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
--- config/production.py
+++
@@ -1,6 +0,0 @@
-import os
-from .local import Config
-
-class ProductionConfig(Config):
-
-    DB_SERVER = '192.168.19.32'
