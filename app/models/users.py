8c271c65860e723a9a04ae3478ff492d5ff0e98f
Minh Nguyen <hongminh1620@gmail.com>
2020-05-21 14:12:42 +0700

    add comments

--- app/models/users.py
+++ app/models/users.py
@@ -1,35 +1,37 @@
-from ..db import db
+from app import db
 from marshmallow import Schema, fields
 
 
 class UserModel(db.Model):
-	__tablename__ = 'users'
+	__tablename__ = "users"
 
+	# Columns
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(80))
 	password = db.Column(db.String(100))
 	salt = db.Column(db.String(100))
 
+	# relationship
 	items = db.relationship('ItemModel')
 
+	# attributes
 	def __init__(self, username, password, salt):
 		self.username = username
 		self.password = password
 		self.salt = salt
 
+	# add object to database
 	def save_to_db(self):
 		db.session.add(self)
 		db.session.commit()
 
+	# query by username
 	@classmethod
 	def find_by_username(cls, username):
 		return cls.query.filter_by(username=username).first()
 
-	@classmethod
-	def find_by_id(cls, _id):
-		return cls.query.filter_by(id=_id).first()
 
-
+# Marshmallow
 class UserSchema(Schema):
 	username = fields.Str()
 	password = fields.Str()
