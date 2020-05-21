e1aea8ba9a93e1f7866cd8167ac7015ef2da0d3f
Minh Nguyen <hongminh1620@gmail.com>
2020-05-21 12:04:25 +0700

    add comment

--- unit_test/test_users.py
+++ unit_test/test_users.py
@@ -1,25 +1,39 @@
+# import fixture function to run test
 from .helpers import client
 
 
 def test_create_user(client):
+    """
+    Test: Create a new user
+    """
+
+    # invalid input
     data = {"username": 230, "password": "abc"}
     rv = client.post('/users', json=data)
     assert rv.status_code == 422
 
+    # create a new user
     data = {"username": "mia", "password": "abc"}
     rv = client.post('/users', json=data)
     assert rv.status_code == 201
 
+    # existing user
     data = {"username": "mia", "password": ""}
     rv = client.post('/users', json=data)
     assert rv.status_code == 400
 
 
 def test_auth(client):
+    """
+    Test: Authorization
+    """
+
+    # valid user
     data = {"username": "mia", "password": "abc"}
     rv = client.post('/auth', json=data)
     assert rv.status_code == 200
 
+    # wrong password
     data = {"username": "mia", "password": "abcd"}
     rv = client.post('/auth', json=data)
     assert rv.status_code == 401
