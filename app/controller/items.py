c47c4d30172f1d8854dca033a0f63760b58fee57
Minh Nguyen <hongminh1620@gmail.com>
2020-05-21 13:50:38 +0700

    add comments

--- app/controller/items.py
+++ app/controller/items.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify, request
 from marshmallow import ValidationError
 from app import db
+
 from ..models.items import ItemModel, ItemSchema
-from ..models.categories import CategorySchema
 from .users import token_required
 
 items = Blueprint('items', __name__)
@@ -15,11 +15,16 @@ def get_items(category_id):
 	output: show all items in that category
 		- each item: name of item
 	"""
+
+	# query all items in that category
 	list_items = db.session.query(ItemModel).filter(ItemModel.category_id == category_id).all()
-	res = []
-	schema = ItemSchema()
-	for i in list_items:
-		res.append(schema.dump(i))
+
+	# create marshmallow instance
+	schema = ItemSchema(many=True)
+
+	# format output by marshmallow
+	res = [schema.dumps(list_items)]
+
 	return jsonify({"categories": res}), 200
 
 
@@ -36,22 +41,28 @@ def create_item(category_id, user_id):
 		- add new item to the category successfully
 		- raise error for existed item
 	"""
+	# take response body
 	data = request.get_json()
 
+	# check the validity of input
 	try:
 		res = ItemSchema().load(data)
 	except ValidationError as err:
 		return jsonify(err.messages), 422
 
+	# get item name and description from request
 	item_name = data['name']
 	item_description = data['description']
 
+	# query item to check the existent of item in a specific category
 	item = db.session.query(ItemModel).filter(ItemModel.category_id == category_id)\
 		.filter(ItemModel.name == item_name).first()
 
+	# existing item
 	if item:
-		return jsonify({'message': 'existed item'}), 400
+		return jsonify({'message': 'Item is existed'}), 400
 
+	# create a new item and save to database
 	new_item = ItemModel(item_name, item_description, category_id, user_id)
 	new_item.save_to_db()
 
@@ -68,10 +79,12 @@ def get_item(category_id, item_id):
 	- item id and description (if found)
 	- raise error if not
 	"""
+	# query based on item id
 	item = db.session.query(ItemModel).filter(ItemModel.id == item_id).first()
-	schema = ItemSchema()
+
+	# check the validity
 	if item:
-		return jsonify(schema.dump(item)), 200
+		return jsonify(ItemSchema().dump(item)), 200
 	else:
 		return jsonify({"message": "item not found"}), 404
 
@@ -83,14 +96,25 @@ def delete_item(category_id, item_id, user_id):
 	input: item_id, category_id
 	output: delete from item list
 	"""
+
+	# query the item based on item id
 	item = db.session.query(ItemModel).filter(ItemModel.id == item_id).first()
 
+	# if item is in the database
 	if item:
+
+		# authorized
 		if item.user_id == user_id:
+
+			# delete item from db
 			item.delete_from_db()
 			return jsonify({"message": "deleted!"}), 200
+
+		# unauthorized
 		else:
 			return jsonify({"message": "Unauthorized"}), 401
+
+	# item not found
 	else:
 		return jsonify({"message": "item not found"}), 404
 
@@ -104,22 +128,37 @@ def edit_item(category_id, item_id, user_id):
 		- updated description (if existed)
 		- raise error if not existed
 	"""
+	# take json body of request
 	data = request.get_json()
 
+	# check the validity of input
 	try:
 		ItemSchema().load(data)
 	except ValidationError as err:
 		return jsonify(err.messages), 422
 
+	# information - new description
 	description = data['description']
 
+	# query the target item
 	item = db.session.query(ItemModel).filter(ItemModel.id == item_id).first()
-	schema = ItemSchema()
+
+	# if found item
 	if item:
+
+		# authorized
 		if item.user_id == user_id:
+
+			# update
 			item.description = description
-			return jsonify(schema.dump(item)), 200
+
+			# format output
+			return jsonify(ItemSchema().dump(item)), 200
+
+		# unauthorized
 		else:
 			return jsonify({"message": "Unauthorized"}), 401
+
+	# item not found
 	else:
 		return jsonify({"message": "item not found"}), 404
