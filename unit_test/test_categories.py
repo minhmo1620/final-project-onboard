1cd100c8fbdbbb810eb5615418b0b4e40cfcf6df
Minh Nguyen <hongminh1620@gmail.com>
2020-05-21 12:06:21 +0700

    add comment

--- unit_test/test_categories.py
+++ unit_test/test_categories.py
@@ -1,19 +1,30 @@
+# import fixture function to run test
 from .helpers import client
 
 
 def test_get_categories(client):
+    """
+    Test: Get all categories in the catalog
+    """
     rv = client.get('/categories')
     assert rv.status_code == 200
 
 
 def test_create_category(client):
+    """
+    Test: Create a new category
+    """
+
+    # create a new category
     data = {"name": "mia", "description": "abc"}
     rv = client.post('/categories', json=data)
     assert rv.status_code == 201
 
+    # existing category
     rsp = client.post('/categories', json=data)
     assert rsp.status_code == 400
 
+    # invalid input
     data = {"name": 123, "description": "abc"}
     rv = client.post('/categories', json=data)
     assert rv.status_code == 422
