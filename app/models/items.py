44033eb03a55baf89ef9188f92a4a1a861d3663f
Minh Nguyen <hongminh1620@gmail.com>
2020-05-21 14:15:53 +0700

    add comments

--- app/models/items.py
+++ app/models/items.py
@@ -1,9 +1,10 @@
-from ..db import db
+from app import db
 from marshmallow import Schema, fields
 
 class ItemModel(db.Model):
 	__tablename__ = "items"
 
+	# Columns
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(100))
 	description = db.Column(db.String(200))
@@ -16,22 +17,18 @@ class ItemModel(db.Model):
 		self.category_id = category_id
 		self.user_id = user_id
 
-	def json(self):
-		return {'name': self.name, 'description': self.description}
-
-	@classmethod
-	def find_by_name(cls, name):
-		return cls.query.filter_by(name=name).first() 
-
+	# add a new object to database
 	def save_to_db(self):
 		db.session.add(self)
 		db.session.commit()
 
+	# delete object from database
 	def delete_from_db(self):
 		db.session.delete(self)
 		db.session.commit()
 
 
+# Marshmallow
 class ItemSchema(Schema):
 	name = fields.Str()
 	description = fields.Str()
