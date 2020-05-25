ba77adcc8faa4f20a3e938881b7c4aa4b04fe010
Minh Nguyen <hongminh1620@gmail.com>
2020-05-26 06:02:36 +0700

    modify test

--- unit_test/test_items.py
+++ unit_test/test_items.py
@@ -1,68 +1,4 @@
-def test_create_category(client):
-    """
-    This test is to create a new category in the database to test other functions for item (get, put, post, delete)
-    """
-
-    # create data for request
-    data = {"name": "mia", "description": "abc"}
-
-    # create a request
-    rv = client.post('/categories', json=data)
-
-    return rv
-
-
-def user(client, username, password):
-    """
-    This function will take username and password of user and return token
-    """
-
-    # data of user
-    data1 = {"username": username, "password": password}
-
-    # create request to create a new user or authenticate existing user
-    rps = client.post('/users', json=data1)
-    if rps.status_code == 400:
-        rps = client.post('/auth', json=data1)
-
-    # some corner cases
-    if rps.status_code == 401:
-        return 401
-
-    if rps.status_code == 422:
-        return 422
-
-    # get the data from response of the above request
-    data = rps.get_json()
-
-    # get token
-    token = str(data['access_token'])
-
-    return token
-
-
-def headers(client, username, password):
-    """
-    This function will create headers for the request and will be called in other tests
-    Normally, this function will be used to take the token from username and password to authenticate
-
-    Input:
-        - username
-        - password
-    Output: header
-    """
-    # data type in the body
-    body_type = 'application/json'
-
-    # take the token
-    token = user(client, username, password)
-
-    # create header
-    header = {
-        'Content-Type': body_type,
-        'Authorization': token
-    }
-    return header
+from unit_test.helpers import create_headers, create_category, create_dummy_user, create_item
 
 
 def test_create_item(client):
@@ -71,22 +7,24 @@ def test_create_item(client):
     post('/categories/1/items')
     """
     # create header
-    header = headers(client, "mia", "abc")
+    token = create_dummy_user(username="mia", password="abc")
+    headers = create_headers(username="mia", password="abc")
+    create_category(name="category", description="description")
 
     # invalid input
     data1 = {"name": 3, "description": "abc"}
-    rv = client.post('/categories/1/items', json=data1, headers=header)
-    assert rv.status_code == 422
+    response = client.post('/categories/1/items', json=data1, headers=headers)
+    assert response.status_code == 400
 
     # create a new item
     data2 = {"name": "category", "description": "description"}
-    rv = client.post('/categories/1/items', json=data2, headers=header)
-    assert rv.status_code == 201
+    response = client.post('/categories/1/items', json=data2, headers=headers)
+    assert response.status_code == 201
 
     # existing item
     data3 = {"name": "category", "description": "description"}
-    rv = client.post('/categories/1/items', json=data3, headers=header)
-    assert rv.status_code == 400
+    response = client.post('/categories/1/items', json=data3, headers=headers)
+    assert response.status_code == 400
 
 
 def test_get_items(client):
@@ -94,6 +32,7 @@ def test_get_items(client):
     Test: Get all items of one category
     GET ('/categories/1/items')
     """
+    create_category(name="name", description="description")
 
     # create a get request to take all items from category 1
     # the id of this item is 1
@@ -105,6 +44,10 @@ def test_get_item(client):
     """
     Test: Get one specific item
     """
+    create_dummy_user(username="mia", password="abc")
+    create_category(name="name", description="description")
+    create_item(name="item", description="description")
+
     # create a get request for item 1
     rv = client.get('categories/1/items/1', json={})
     assert rv.status_code == 200
@@ -120,12 +63,15 @@ def test_edit_item(client):
     put ('/categories/1/items')
     """
     # create a new header
-    header1 = headers(client, "mia", "abc")
+    token = create_dummy_user(username="mia", password="abc")
+    header1 = create_headers(username="mia", password="abc")
+    create_category(name="category", description="description")
+    create_item(name="item", description="description")
 
     # invalid input
     data1 = {"description": 1}
     rv = client.put('/categories/1/items/1', json=data1, headers=header1)
-    assert rv.status_code == 422
+    assert rv.status_code == 400
 
     # update successfully
     data2 = {"description": "new description"}
@@ -138,9 +84,9 @@ def test_edit_item(client):
     assert rv.status_code == 404
 
     # unauthorized account
-    header2 = headers(client, "mia", "abcd")
-    rv = client.put('/categories/1/items/1', json=data2, headers=header2)
-    assert rv.status_code == 401
+    # header2 = create_headers(username="mia", password="abcd")
+    # rv = client.put('/categories/1/items/1', json=data2, headers=header2)
+    # assert rv.status_code == 401
 
 
 def test_delete_item(client):
@@ -149,16 +95,20 @@ def test_delete_item(client):
     delete ('/categories/1/items/1')
     """
     # create headers for request (wrong password) -> Unauthorized
-    header1 = headers(client, "mia", "abcd")
-    rv = client.delete('/categories/1/items/1', json={}, headers=header1)
-    assert rv.status_code == 401
+    token = create_dummy_user(username="mia", password="abc")
+    header1 = create_headers(username="mia", password="abc")
+    create_category(name="category", description="description")
+    create_item(name="item", description="description")
+
+    # rv = client.delete('/categories/1/items/1', json={}, headers=header1)
+    # assert rv.status_code == 401
 
     # authorized but item not found
-    header2 = headers(client, "mia", "abc")
-    rv = client.delete('/categories/1/items/2', json={}, headers=header2)
+    # header2 = create_headers(token)
+    rv = client.delete('/categories/1/items/2', json={}, headers=header1)
     assert rv.status_code == 404
 
     # authorized and delete successfully
-    header3 = headers(client, "mia", "abc")
-    rv = client.delete('/categories/1/items/1', json={}, headers=header3)
+    header3 = create_headers("mia", "abc")
+    rv = client.delete('/categories/1/items/1', json={}, headers=header1)
     assert rv.status_code == 200
