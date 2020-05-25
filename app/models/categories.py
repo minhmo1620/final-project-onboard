1b15d2f151b654177d8fff0f749ac672369c6f23
Minh Nguyen <hongminh1620@gmail.com>
2020-05-25 15:29:57 +0700

    category model

--- app/models/categories.py
+++ app/models/categories.py
@@ -1,29 +1,15 @@
-from marshmallow import Schema, fields
 from ..db import db
 
 
 class CategoryModel(db.Model):
     __tablename__ = 'categories'
 
-    # Columns
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     description = db.Column(db.String(100))
 
-    # relationship
     items = db.relationship('ItemModel')
 
     def __init__(self, name, description):
         self.name = name
         self.description = description
-
-    # query by username
-    @classmethod
-    def find_by_name(cls, name):
-        return cls.query.filter_by(name=name).first()
-
-
-# Marshmallow
-class CategorySchema(Schema):
-    name = fields.Str()
-    description = fields.Str()
