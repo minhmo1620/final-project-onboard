2c108aff0ac4ae2a49d2d900ea49ea71d44bbc74
Minh Nguyen <hongminh1620@gmail.com>
2020-05-25 15:28:12 +0700

    delete comments and modify status code

--- app/helpers.py
+++ app/helpers.py
@@ -6,19 +6,15 @@ import jwt
 from flask import jsonify, request
 from marshmallow import ValidationError
 
-from app.models.users import UserModel, UserSchema
-from app.models.items import ItemSchema
-from app.models.categories import CategorySchema
+from app.models.users import UserModel
+from .db import db
 
 # take secret key from .env
 secret_key = str(os.getenv('SECRET_KEY'))
 
 
-def hash_password(users_password):
-    """
-    hash user's password
-    """
-    return hashlib.sha256(users_password.encode('utf-8')).hexdigest()
+def hash_password(user_password):
+    return hashlib.sha256(user_password.encode('utf-8')).hexdigest()
 
 
 def token_required(f):
@@ -27,38 +23,37 @@ def token_required(f):
     """
 
     @wraps(f)
-    def decorator(*arg, **kwargs):
+    def wrapper(*arg, **kwargs):
         token = request.headers["Authorization"].split()
         if token[0] != "Bearer":
-            return jsonify({"message": "Invalid token"}), 401
+            return jsonify({"message": "Invalid token"}), 403
         try:
             data = jwt.decode(token[1], secret_key)
             username = data['user']
-            user = UserModel.find_by_username(username)
+            user = db.session.query(UserModel).filter_by(username=username).first()
+            if user is None:
+                return jsonify({'message': 'Unauthenticated'}), 401
+
         except:
-            return jsonify({'message': 'Token is invalid'}), 401
+            return jsonify({'message': 'Token is invalid'}), 40
         return f(*arg, **kwargs, user_id=user.id)
 
-    return decorator
+    return wrapper
 
 
 def validate_input(schema):
-    def function(f):
+    def decorator(f):
         @wraps(f)
-        def decorator(*args, **kwargs):
+        def wrapper(*args, **kwargs):
             # take the input
             data = request.get_json()
 
             # check the validity of the input
             try:
-                if schema == "item":
-                    ItemSchema().load(data)
-                elif schema == "category":
-                    CategorySchema().load(data)
-                elif schema == "user":
-                    UserSchema().load(data)
+                schema().load(data)
             except ValidationError as err:
-                return jsonify(err.messages), 422
-            return f(*args, **kwargs)
-        return decorator
-    return function
+                return jsonify(err.messages), 400
+            return f(*args, **kwargs, data=data)
+        return wrapper
+    return decorator
+
