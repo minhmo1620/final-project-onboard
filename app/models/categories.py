4ce36c24c749f672255339d69980a291be8a8741
Minh Nguyen <hongminh1620@gmail.com>
2020-05-26 05:55:42 +0700

    delete redundant relationship

--- app/models/categories.py
+++ app/models/categories.py
@@ -2,13 +2,13 @@ from ..db import db
 
 
 class CategoryModel(db.Model):
-    __tablename__ = 'categories'
+    __tablename__ = "categories"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     description = db.Column(db.String(100))
 
-    items = db.relationship('ItemModel')
+    items = db.relationship("ItemModel")
 
     def __init__(self, name, description):
         self.name = name
