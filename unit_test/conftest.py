1970b2171576f429442d90b002f85f42ec5b9905
Minh Nguyen <hongminh1620@gmail.com>
2020-05-25 15:10:24 +0700

    update client()

--- unit_test/conftest.py
+++ unit_test/conftest.py
@@ -9,14 +9,6 @@ def client():
     client = app.test_client()
 
     with app.app_context():
-        from app.controllers.items import items_blueprint
-        from app.controllers.categories import categories_blueprint
-        from app.controllers.users import users_blueprint
-
-        app.register_blueprint(categories_blueprint, url_prefix="/")
-        app.register_blueprint(items_blueprint, url_prefix="/")
-        app.register_blueprint(users_blueprint, url_prefix="/")
-
         db.drop_all()
         db.create_all()
 
