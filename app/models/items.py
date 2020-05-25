12b3e770db3daacec16fb06dc9d0cc3ccd5cdf5d
Minh Nguyen <hongminh1620@gmail.com>
2020-05-26 05:57:03 +0700

    add some requirements for attributes

--- app/models/items.py
+++ app/models/items.py
@@ -7,8 +7,8 @@ class ItemModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     description = db.Column(db.String(200))
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
     def __init__(self, name, description, category_id, user_id):
         self.name = name
