49ffd384173953313172387f1a0ddc06b531a904
Minh Nguyen <hongminh1620@gmail.com>
2020-05-25 15:28:49 +0700

    Item Schema

--- app/schemas/items.py
+++ app/schemas/items.py
@@ -1,6 +1,15 @@
 from marshmallow import Schema, fields, validate
 
 
+class CreateItem(Schema):
+    name = fields.Str(required=True,
+                      validate=validate.Length(max=100))
+    description = fields.Str(required=True,
+                             validate=validate.Length(max=200))
+
+
 class ItemSchema(Schema):
-    name = fields.Str(required=True)
-    description = fields.Str(required=True)
+    name = fields.Str(required=False,
+                      validate=validate.Length(max=100))
+    description = fields.Str(required=True,
+                             validate=validate.Length(max=200))
