a0ab3f86d71f0f0d916ba41f170c4183b06763f1
Minh Nguyen <hongminh1620@gmail.com>
2020-05-25 14:38:45 +0700

    update items endpoints with two schemas

--- app/controllers/items.py
+++ app/controllers/items.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
+
 from app import db
 from app.helpers import validate_input, token_required
-from app.models.items import ItemModel, ItemSchema
+from app.models.items import ItemModel
 from app.models.categories import CategoryModel
+from app.schemas.items import ItemSchema, CreateItem
 
 items_blueprint = Blueprint('items', __name__)
 
@@ -14,22 +16,21 @@ def get_items(category_id):
     output: show all items in that category
         - each item: name of item
     """
+    # check the category based on category_id
+    category = db.session.query(CategoryModel).filter(CategoryModel.id == category_id).first()
+    if not category:
+        return jsonify({"message": "Category not found"}), 404
 
     # query all items in that category
     list_items = db.session.query(ItemModel).filter(ItemModel.category_id == category_id).all()
 
-    # format output by marshmallow
-    res = ItemSchema(many=True).dump(list_items)
-
-    # category name
-    category = db.session.query(CategoryModel).filter(CategoryModel.id == category_id).first().name
-    return jsonify({str(category): res}), 200
+    return jsonify(ItemSchema(many=True).dump(list_items)), 200
 
 
 @items_blueprint.route('/categories/<int:category_id>/items', methods=['POST'])
 @token_required
-@validate_input(schema="item")
-def create_item(category_id, user_id):
+@validate_input(schema=CreateItem)
+def create_item(category_id, user_id, data):
     """
     input:
         - category_id (int)
@@ -40,104 +41,118 @@ def create_item(category_id, user_id):
         - add new item to the category successfully
         - raise error for existed item
     """
-    # take response body
-    data = request.get_json()
+    # check the category based on category_id
+    category = db.session.query(CategoryModel).filter(CategoryModel.id == category_id).first()
+    if not category:
+        return jsonify({"message": "Category not found"}), 404
 
-    # get item name and description from request
-    item_name = data['name']
-    item_description = data['description']
+    name = data["name"]
+    description = data["description"]
 
-    # query item to check the existent of item in a specific category
     item = db.session.query(ItemModel).filter(ItemModel.category_id == category_id)\
-        .filter(ItemModel.name == item_name).first()
+        .filter(ItemModel.name == name).first()
 
     # existing item
     if item:
-        return jsonify({'message': 'Item is existed'}), 400
+        return jsonify({"message": "Item is existed"}), 400
 
     # create a new item and save to database
-    new_item = ItemModel(item_name, item_description, category_id, user_id)
+    new_item = ItemModel(name, description, category_id, user_id)
     db.session.add(new_item)
     db.session.commit()
 
-    return jsonify({'message': 'created!'}), 201
+    return jsonify({"message": "Created item successfully"}), 201
 
 
-@items_blueprint.route('/categories/<int:category_id>/items/<int:item_id>', methods=['GET'])
-def get_item(item_id, **__):
+@items_blueprint.route("/categories/<int:category_id>/items/<int:item_id>", methods=["GET"])
+def get_item(item_id, category_id):
     """
     input:
         - category_id (int)
         - item_id (int)
     output:
     - item id and description (if found)
-    - raise error if not
+    - return error if not
     """
+    # check the category based on category_id
+    category = db.session.query(CategoryModel).filter(CategoryModel.id == category_id).first()
+    if not category:
+        return jsonify({"message": "Category not found"}), 404
+
     # query based on item id
-    item = db.session.query(ItemModel).filter(ItemModel.id == item_id).first()
+    item = db.session.query(ItemModel).filter(ItemModel.category_id == category_id) \
+        .filter(ItemModel.id == item_id).first()
 
     # check the validity
     if not item:
-        return jsonify({"message": "item not found"}), 404
+        return jsonify({"message": "Item not found"}), 404
     return jsonify(ItemSchema().dump(item)), 200
 
 
-@items_blueprint.route('/categories/<int:category_id>/items/<int:item_id>', methods=['DELETE'])
+@items_blueprint.route("/categories/<int:category_id>/items/<int:item_id>", methods=["DELETE"])
 @token_required
-def delete_item(item_id, user_id, **__):
+def delete_item(item_id, user_id, category_id):
     """
     input: item_id, category_id
     output: delete from item list
     """
+    # check the category of item
+    category = db.session.query(CategoryModel).filter(CategoryModel.id == category_id).first()
+    if not category:
+        return jsonify({"message": "Category not found"}), 404
 
-    # query the item based on item id
-    item = db.session.query(ItemModel).filter(ItemModel.id == item_id).first()
+    item = db.session.query(ItemModel).filter(ItemModel.category_id == category_id) \
+        .filter(ItemModel.id == item_id).first()
 
     # if item is in the database
     if not item:
-        return jsonify({"message": "item not found"}), 404
+        return jsonify({"message": "Item not found"}), 404
 
     # unauthorized
     if item.user_id != user_id:
-        return jsonify({"message": "Unauthorized"}), 401
+        return jsonify({"message": "Unauthorized"}), 403
 
     # authorized
     db.session.delete(item)
     db.session.commit()
 
-    return jsonify({"message": "deleted!"}), 200
+    return jsonify({"message": "Deleted item successfully"}), 200
 
 
 @items_blueprint.route('/categories/<int:category_id>/items/<int:item_id>', methods=['PUT'])
 @token_required
-@validate_input(schema="item")
-def edit_item(item_id, user_id, **__):
+@validate_input(schema=ItemSchema)
+def edit_item(item_id, user_id, data, category_id):
     """
     input: item_id
     output:
         - updated description (if existed)
         - raise error if not existed
     """
-    # take json body of request
-    data = request.get_json()
+    # check the category of item
+    category = db.session.query(CategoryModel).filter(CategoryModel.id == category_id).first()
+    if not category:
+        return jsonify({"message": "Category not found"}), 404
 
     # information - new description
     description = data['description']
 
     # query the target item
-    item = db.session.query(ItemModel).filter(ItemModel.id == item_id).first()
+    item = db.session.query(ItemModel).filter(ItemModel.category_id == category_id) \
+        .filter(ItemModel.id == item_id).first()
 
     # item not found
     if not item:
-        return jsonify({"message": "item not found"}), 404
+        return jsonify({"message": "Item not found"}), 404
 
     # unauthorized
     if item.user_id != user_id:
-        return jsonify({"message": "Unauthorized"}), 401
+        return jsonify({"message": "Unauthorized"}), 403
 
     # authorized
     # update
     item.description = description
+    db.session.commit()
 
     # format output
     return jsonify(ItemSchema().dump(item)), 200
