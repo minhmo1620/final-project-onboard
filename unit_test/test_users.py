733e8fa7c5c58f080b2a1740727cab9ff433c34e
Minh Nguyen <hongminh1620@gmail.com>
2020-05-26 06:47:59 +0700

    add more assert

--- unit_test/test_users.py
+++ unit_test/test_users.py
@@ -1,3 +1,5 @@
+from flask import json
+
 from unit_test.helpers import create_dummy_user
 
 
@@ -10,16 +12,28 @@ def test_create_user(client):
     data = {"username": 230, "password": "abc"}
     response = client.post('/users', json=data)
     assert response.status_code == 400
+    expected_result = {'username': ['Not a valid string.']}
+    assert expected_result == json.loads(response.data)
 
     # create a new user
     data = {"username": "mia", "password": "abc"}
     response = client.post("/users", json=data)
     assert response.status_code == 201
+    assert "access_token" in json.loads(response.data)
+
+    # existing user
+    data = {"username": "mia", "password": "abcd"}
+    response = client.post("/users", json=data)
+    assert response.status_code == 400
+    expected_result = {"message": "existed username"}
+    assert expected_result == json.loads(response.data)
 
     # existing user
     data = {"username": "mia", "password": ""}
     response = client.post("/users", json=data)
     assert response.status_code == 400
+    expected_result = {"password": ["Length must be between 1 and 100."]}
+    assert expected_result == json.loads(response.data)
 
 
 def test_auth(client):
@@ -32,8 +46,11 @@ def test_auth(client):
     data = {"username": "mia", "password": "abc"}
     response = client.post("/auth", json=data)
     assert response.status_code == 200
+    assert "access_token" in json.loads(response.data)
 
     # wrong password
     data = {"username": "mia", "password": "abcd"}
     response = client.post("/auth", json=data)
     assert response.status_code == 401
+    expected_result = {"message": "wrong password"}
+    assert expected_result == json.loads(response.data)
