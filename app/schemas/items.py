d8190a7cce735826494e4fd44364f50e53acc150
Minh Nguyen <hongminh1620@gmail.com>
2020-05-26 16:14:05 +0700

    fix required = True

--- app/schemas/items.py
+++ app/schemas/items.py
@@ -8,7 +8,7 @@ class UpdateItem(Base):
 
 
 class ItemSchema(Base):
-    name = fields.Str(required=False,
+    name = fields.Str(required=True,
                       validate=validate.Length(max=100, min=1))
     description = fields.Str(required=True,
                              validate=validate.Length(max=200, min=1))
