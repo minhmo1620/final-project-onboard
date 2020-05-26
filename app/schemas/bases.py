f3e9a3f647f7b0e78b9449f393289039a2d51cb1
Minh Nguyen <hongminh1620@gmail.com>
2020-05-26 16:15:02 +0700

    change to isinstance

--- app/schemas/bases.py
+++ app/schemas/bases.py
@@ -5,6 +5,6 @@ class Base(Schema):
     @pre_load
     def strip_data(self, data, **kwargs):
         for key, value in data.items():
-            if type(value) == str:
+            if isinstance(value, str):
                 data[key] = value.strip()
         return data
