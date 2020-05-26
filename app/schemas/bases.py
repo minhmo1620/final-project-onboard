991ebee637a164339302b9193f15322e795d0f0a
Minh Nguyen <hongminh1620@gmail.com>
2020-05-26 17:24:42 +0700

    delete kwargs

--- app/schemas/bases.py
+++ app/schemas/bases.py
@@ -3,7 +3,7 @@ from marshmallow import Schema, pre_load
 
 class Base(Schema):
     @pre_load
-    def strip_data(self, data, **kwargs):
+    def strip_data(self, data):
         for key, value in data.items():
             if isinstance(value, str):
                 data[key] = value.strip()
